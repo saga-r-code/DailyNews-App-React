99992c21ab239cbbe58e5261d7f5bd5f4366078c
saga-r-code <shah.sagar2006@gmail.com>
2024-06-15 03:50:39 +0530

    first commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,11 +6,11 @@ export class NavBar extends Component {
       <div>
          <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-2 px-4 text-lg text-gray-700 b">
   <div className="flex items-center">
-  <svg class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <rect x="3" y="3" width="18" height="18" rx="2" ry="2" fill="#f5f5f5" stroke="#333" stroke-width="2"/>
-  <path d="M6 8h12" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M6 12h6" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M14 16h4" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <rect x="3" y="3" width="18" height="18" rx="2" ry="2" fill="#f5f5f5" stroke="#333" strokeWidth="2"/>
+  <path d="M6 8h12" stroke="#333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M6 12h6" stroke="#333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M14 16h4" stroke="#333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   <rect x="14" y="12" width="4" height="4" fill="#333" rx="1" ry="1"/>
 </svg>
 
@@ -33,7 +33,7 @@ export class NavBar extends Component {
   </ul>
   <button className="md:hidden flex items-center px-3 py-2 rounded text-gray-600 hover:text-gray-900" id="menu-button">
     <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
     </svg>
   </button>
 </nav>
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-  let {title, description} = this.props
+  let {title, description, imageUrl, newsUrl} = this.props
     return (
       <div className="relative flex w-full max-w-[26rem] mt-4 flex-col rounded-xl bg-white text-gray-700 shadow-lg">
       <div className="relative mx-4 mt-4 overflow-hidden rounded-xl bg-blue-gray-500 text-white shadow-lg">
         <img
-          src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
+        src={imageUrl}
           alt="Wooden House, Florida"
         />
         <div className="absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60"></div>
@@ -30,7 +30,19 @@ export class Newsitem extends Component {
          {description}
         </p>
     
-        
+        <div className="my-3">
+        <a href={newsUrl} rel="noreferrer" target="_blank"
+							className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+							Read more
+							<svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg">
+								<path fillRule="evenodd"
+									d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+									clipRule="evenodd"></path>
+							</svg>
+						</a>
+        </div>
+
       </div>
     </div>
     
