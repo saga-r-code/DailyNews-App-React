06514f7210bb42aac9063fa7cfc3de7f622d1b28
saga-r-code <shah.sagar2006@gmail.com>
2024-06-19 21:40:25 +0530

    first commit

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   article = []; //constuctor throgh give data in the array
@@ -16,56 +17,87 @@ export class News extends Component {
 
   //Fetching the Api and pass data and update data throgh this.setState
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=479ac70180ff4770a28cd9a4c23752c6&page=1pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=479ac70180ff4770a28cd9a4c23752c6&page=1&pageSize=${this.props.pageSize}`;
+
+    this.setState({ loading: true });
+
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ article: parsedData.articles , totalResults : parsedData.totalResults}); // pass to constuctor in article : this.article
+
+    this.setState({
+      article: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    }); // pass to constuctor in article : this.article
   }
 
-  handlePrevClick = async ()=>{
-    console.log("previous")
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=479ac70180ff4770a28cd9a4c23752c6&page=${this.state.page - 1}&pageSize=20`;
+  handlePrevClick = async () => {
+    console.log("previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=479ac70180ff4770a28cd9a4c23752c6&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ 
-      page : this.state.page - 1,
-      article: parsedData.articles 
+
+    this.setState({
+      page: this.state.page - 1,
+      article: parsedData.articles,
+      loading: false,
     });
+  };
 
-  }
+  handleNextClick = async () => {
+    console.log("next");
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=479ac70180ff4770a28cd9a4c23752c6&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
 
-  handleNextClick = async ()=>{
-    console.log("next")  
-    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+      this.setState({ loading: true });
 
-    }
-    else{
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=479ac70180ff4770a28cd9a4c23752c6&page=${this.state.page + 1}&pageSize=20`;
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
-      this.setState({ 
-        page : this.state.page + 1,
-        article: parsedData.articles 
+
+      this.setState({
+        page: this.state.page + 1,
+        article: parsedData.articles,
+        loading: false,
       });
     }
-  }
+  };
 
   render() {
     return (
       <div className="container p-2 my-5 2xl:w-[80%] 2xl:m-auto">
         <h1 className="text-3xl font-bold">DailyNews - Top Headlines</h1>
+
+        {/* when loading is true than show otherwise not show */}
+        {this.state.loading && <Spinner />}
+        {/* <Spinner /> */}
+        
+
         <div className="cards grid md:grid-cols-2 md:gap-x-4 lg:grid-cols-3 2xl:grid-cols-4">
-          {this.state.article.map((elem) => {
-            //show all ddescription on the web page
+          {!this.state.loading && this.state.article.map((elem) => {
+            //show all description on the web page
             return (
               <Newsitem
                 key={elem.url}
                 title={elem.title ? elem.title : " "}
                 description={elem.description ? elem.description : " "}
-                imageUrl={!elem.urlToImage ? "https://wallpapers.com/images/featured/newspaper-aesthetic-6zy4tj738voyh9fx.jpg" : elem.urlToImage}
+                imageUrl={
+                  !elem.urlToImage
+                    ? "https://wallpapers.com/images/featured/newspaper-aesthetic-6zy4tj738voyh9fx.jpg"
+                    : elem.urlToImage
+                }
                 newsUrl={elem.url}
               />
             );
@@ -76,11 +108,26 @@ export class News extends Component {
         </div>
 
         <div className="flex justify-between items-center w-[80%] m-auto mt-10">
-          <button  disabled={this.state.page <= 1}  className="flex items-center py-2 px-3  font-medium select-none border  text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-         onClick={this.handlePrevClick}> ⪻ Previous </button>
+          <button
+            disabled={this.state.page <= 1}
+            className="flex items-center py-2 px-3  font-medium select-none border  text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:cursor-not-allowed disabled:bg-slate-600"
+            onClick={this.handlePrevClick}
+          >
+            {" "}
+            ⪻ Previous{" "}
+          </button>
 
-          <button className="flex items-center py-2 px-3  font-medium select-none border text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-             onClick={this.handleNextClick}> Next ⪼</button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            className="flex items-center py-2 px-3  font-medium select-none border text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:cursor-not-allowed  disabled:bg-slate-600"
+            onClick={this.handleNextClick}
+          >
+            {" "}
+            Next ⪼
+          </button>
         </div>
       </div>
     );
